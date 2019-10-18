903f18fb9cd4bc7bf8daf5bc812cb76c3dc368f0
Nabeelah <noblenab13@gmail.com>
2019-10-18 10:02:17 +0100

    Solve  name compare with BST

--- names/names.py
+++ names/names.py
@@ -1,3 +1,4 @@
+from binary_search_tree import BinarySearchTree
 import time
 
 start_time = time.time()
@@ -11,12 +12,22 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+bst1 = BinarySearchTree(names_1[0])
+
+for i in names_1[1:]:
+    bst1.insert(i)
+
+for i in names_2:
+    if bst1.contains(i):
+        duplicates.append(i)
+
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
